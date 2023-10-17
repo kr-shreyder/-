2d8f853ff651bf6a988f5b9044ecee89a9a5cb38
rickrockk <78549183+rickrockk@users.noreply.github.com>
2023-10-17 16:49:05 +0300

    сборка react-проекта на webpack

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss'
+import React from 'react'
+
+const App = () => {
+    return (
+        <div className='app'>
+            продам арбузы
+        </div>
+    );
+};
+
+export default App;
