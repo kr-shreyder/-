31476a17d9b24877a6af4c27b3d3175476b39a13
“akiverin” <kiverin03@yandex.ru>
2023-11-26 00:12:37 +0300

    добавлена боковая панель для фильтрации игр – сверстана

---
+++ frontend/src/views/Catalog/AsideSettings/AsideSettings.jsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import './AsideSettings.scss'
+import starMini1 from '@assets/decorate-star-mini.svg'
+import starMini2 from '@assets/decorate-star-mini2.svg'
+
+const genres = [
+    {
+        name: 'Шутеры',
+        url: '/',
+    },
+    {
+        name: 'Карточные',
+        url: '/',
+    },
+    {
+        name: 'Стратегии',
+        url: '/',
+    },
+    {
+        name: 'Приключения',
+        url: '/',
+    },
+    {
+        name: 'Ролевые',
+        url: '/',
+    },
+    {
+        name: 'Пазлы',
+        url: '/',
+    },
+    {
+        name: 'Спорт',
+        url: '/',
+    },
+    {
+        name: 'Экшены',
+        url: '/',
+    },
+]
+const kits = [
+    {
+        name: 'Рейтинг',
+        url: '/',
+    },
+    {
+        name: 'Просмотры',
+        url: '/',
+    },
+    {
+        name: 'Отзывы',
+        url: '/',
+    },
+    {
+        name: 'MosPolyJam',
+        url: '/',
+    },
+    {
+        name: 'Последние',
+        url: '/',
+    },
+    {
+        name: 'Windows',
+        url: '/',
+    },
+    {
+        name: 'Web',
+        url: '/',
+    },
+    {
+        name: 'Mobile',
+        url: '/',
+    },
+]
+
+function AsideSettings() {
+    return (  
+        <aside className="aside-settings">
+            <ul className="aside-settings__list">
+                <img src={starMini1} alt="" className="aside-settings__icon" />
+                <li className="aside-settings__item">
+                    <h5 className="aside-settings__title">Жанры</h5>
+                </li>
+                {genres.map((item, index) => (
+                    <li className="aside-settings__item" key={index}>
+                        <a href={item.url} className="aside-settings__link">{item.name}</a>
+                    </li>
+                ))}
+            </ul>
+            <ul className="aside-settings__list">
+                <img src={starMini2} alt="" className="aside-settings__icon" />
+                <li className="aside-settings__item">
+                    <h5 className="aside-settings__title">Наборы</h5>
+                </li>
+                {kits.map((item, index) => (
+                    <li className="aside-settings__item" key={index}>
+                        <a href={item.url} className="aside-settings__link">{item.name}</a>
+                    </li>
+                ))}
+            </ul>
+        </aside>
+    );
+}
+
+export default AsideSettings;
--- frontend/src/views/Catalog/Catalog.jsx
+++ frontend/src/views/Catalog/Catalog.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Catalog.scss'
 import AdSlider from './AdSlider/AdSlider';
+import AsideSettings from './AsideSettings/AsideSettings';
 function Catalog() {
     return (  
         <main className="catalog">
             <div className="catalog__wrapper">
+                <AsideSettings />
                 <AdSlider />
             </div>
         </main>
